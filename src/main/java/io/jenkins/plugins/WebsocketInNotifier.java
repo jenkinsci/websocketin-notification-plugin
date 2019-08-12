03c3f091ac2ff64d3c8f0f2d556d945c2e66e6de
Daniel Hobi <info@danielhobi.ch>
2019-08-12 16:49:38 +0200

    HOSTING-821 - Websocket.in notification -> rename plugin to websocketin-notification (removing the dot) -> make security check in doTestConnection -> change to SimpleBuildStep for jenkins pipeline usage

--- src/main/java/io/jenkins/plugins/WebsocketInNotifier.java
+++ src/main/java/io/jenkins/plugins/WebsocketInNotifier.java
@@ -2,17 +2,22 @@ package io.jenkins.plugins;
 
 import hudson.EnvVars;
 import hudson.Extension;
+import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.*;
 import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
 import hudson.util.FormValidation;
 import jenkins.model.Jenkins;
+import jenkins.tasks.SimpleBuildStep;
 import net.sf.json.JSONObject;
+import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.URI;
@@ -21,14 +26,14 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
-public class WebsocketInNotifier extends Notifier {
+public class WebsocketInNotifier extends Notifier implements SimpleBuildStep {
 
     private final String url;
     private final String additionalHttpHeaders;
     private final String startMessage;
 
     @DataBoundConstructor
-    public WebsocketInNotifier(String url, String startMessage, String additionalHttpHeaders){
+    public WebsocketInNotifier(String url, String startMessage, String additionalHttpHeaders) {
         this.url = url;
         this.startMessage = startMessage;
         this.additionalHttpHeaders = additionalHttpHeaders;
@@ -46,35 +51,18 @@ public class WebsocketInNotifier extends Notifier {
         return startMessage;
     }
 
-    @Override
-    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
-        sendMessageBlocking(build, listener);
-        return true;
-    }
-
-    @Override
-    public boolean prebuild(AbstractBuild<?, ?> build, BuildListener listener) {
-        AbstractBuild<?,?> previousBuild = build.getPreviousBuild();
-        if(previousBuild != null) {
-            sendMessageBlocking(previousBuild, listener);
-        } else {
-            sendMessageBlocking(build, listener);
-        }
-        return true;
-    }
-
-    private void sendMessageBlocking(AbstractBuild<?, ?> build, BuildListener listener) {
-        JSONObject json = getBuildJson(build);
+    private void sendMessageBlocking(Run<?, ?> run, TaskListener listener, boolean isPreBuild) {
+        JSONObject json = getBuildJson(run, isPreBuild);
         listener.getLogger().println("Websocket connection to: " + this.url);
 
         try {
-            EnvVars environment = build.getEnvironment(listener);
+            EnvVars environment = run.getEnvironment(listener);
             String expandedUrl = environment.expand(url);
             String expandedStartMessage = environment.expand(startMessage);
-            String expandedAdditionalHttpHeaders= environment.expand(additionalHttpHeaders);
+            String expandedAdditionalHttpHeaders = environment.expand(additionalHttpHeaders);
 
             WebsocketInClient websocketClient = new WebsocketInClient(new URI(expandedUrl), expandedStartMessage, DescriptorImpl.toMap(expandedAdditionalHttpHeaders));
-            listener.getLogger().println("Connected: "+websocketClient.connectBlocking());
+            listener.getLogger().println("Connected: " + websocketClient.connectBlocking());
             websocketClient.send(json.toString());
             websocketClient.closeBlocking();
         } catch (URISyntaxException | InterruptedException | IOException e) {
@@ -82,37 +70,49 @@ public class WebsocketInNotifier extends Notifier {
         }
     }
 
-    private JSONObject getBuildJson(AbstractBuild<?, ?> build) {
+    private JSONObject getBuildJson(Run<?, ?> run, boolean isPreBuild) {
         String resultStatus = null;
-        Result result = build.getResult();
-        if(!build.isBuilding() && build.getPreviousBuild() != null) {
-            result = build.getPreviousBuild().getResult();
+        Result result = run.getResult();
+
+        if (isPreBuild && run.getPreviousBuild() != null) {
+            result = run.getPreviousBuild().getResult();
         }
-        if(result != null) {
+        if (result != null) {
             resultStatus = result.toString();
         }
 
         String fullUrl = null;
-        if(Jenkins.get().getRootUrl() != null) {
-            fullUrl = Jenkins.get().getRootUrl() + build.getUrl();
+        if (Jenkins.get().getRootUrl() != null) {
+            fullUrl = Jenkins.get().getRootUrl() + run.getUrl();
         }
         return new JSONObject()
-                .element("name", build.getProject().getName())
-                .element("url", build.getProject().getUrl())
+                .element("name", run.getParent().getName())
+                .element("url", run.getParent().getUrl())
                 .element("build", new JSONObject()
                         .element("full_url", fullUrl)
-                        .element("number", build.getNumber())
-                        .element("phase", build.isBuilding() ? "COMPLETED" : "STARTED")
+                        .element("number", run.getNumber())
+                        .element("phase", isPreBuild ? "STARTED" : "COMPLETED")
                         .element("status", resultStatus)
-                        .element("url", build.getUrl())
+                        .element("url", run.getUrl())
                 );
     }
 
-    @Extension // This indicates to Jenkins that this is an implementation of an extension point.
-    public static final class DescriptorImpl  extends BuildStepDescriptor<Publisher> {
+    @Override
+    public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath filePath, @Nonnull Launcher launcher, @Nonnull TaskListener taskListener) {
+        sendMessageBlocking(run, taskListener, false);
+    }
+
+    @Override
+    public boolean prebuild(AbstractBuild<?, ?> build, BuildListener listener) {
+        sendMessageBlocking(build, listener, true);
+        return super.prebuild(build, listener);
+    }
+
+    @Extension
+    @Symbol("websocketin")
+    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
         public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-            // Indicates that this builder can be used with all kinds of project types
             return true;
         }
 
@@ -121,29 +121,29 @@ public class WebsocketInNotifier extends Notifier {
         }
 
         public FormValidation doCheckUrl(@QueryParameter String url) {
-            try{
-                if(!url.startsWith("ws")) {
-                    return FormValidation.error(url+" needs to start with ws:// or wss://");
+            try {
+                if (!url.startsWith("ws")) {
+                    return FormValidation.error(url + " needs to start with ws:// or wss://");
                 }
                 new URI(url);
                 return FormValidation.ok();
-            } catch(Exception ex){
+            } catch (Exception ex) {
                 return FormValidation.error("'" + url + "' is not a valid URL.");
             }
         }
 
         public FormValidation doCheckAdditionalHttpHeaders(@QueryParameter String additionalHttpHeaders) {
-            try{
-                if(additionalHttpHeaders != null && !additionalHttpHeaders.isEmpty()) {
-                    Map<String,String> headers = toMap(additionalHttpHeaders);
-                    if(headers.isEmpty()) {
+            try {
+                if (additionalHttpHeaders != null && !additionalHttpHeaders.isEmpty()) {
+                    Map<String, String> headers = toMap(additionalHttpHeaders);
+                    if (headers.isEmpty()) {
                         return FormValidation.warning("No headers detected");
                     }
                     String headerWord = headers.size() == 1 ? "header" : "headers";
-                    return FormValidation.ok(headers.size() + " additional http "+headerWord+" found.");
+                    return FormValidation.ok(headers.size() + " additional http " + headerWord + " found.");
                 }
                 return FormValidation.ok();
-            } catch(Exception ex){
+            } catch (Exception ex) {
                 return FormValidation.error("Not a valid property format");
             }
         }
@@ -151,30 +151,31 @@ public class WebsocketInNotifier extends Notifier {
         public FormValidation doTestConnection(@QueryParameter("url") final String url,
                                                @QueryParameter("startMessage") final String startMessage,
                                                @QueryParameter("additionalHttpHeaders") final String additionalHttpHeaders
-                                               ) {
+        ) {
+            Jenkins.get().checkPermission(Jenkins.ADMINISTER);
             try {
-                Map<String,String> headers = null;
-                if(additionalHttpHeaders != null && !additionalHttpHeaders.equals("")) {
+                Map<String, String> headers = null;
+                if (additionalHttpHeaders != null && !additionalHttpHeaders.equals("")) {
                     headers = toMap(additionalHttpHeaders);
                 }
                 WebsocketInClient client = new WebsocketInClient(new URI(url), startMessage, headers);
                 client.connectBlocking();
                 boolean isSuccessful = false;
-                if(client.isOpen()) {
+                if (client.isOpen()) {
                     isSuccessful = true;
                 }
                 client.closeBlocking();
-                if(isSuccessful) {
+                if (isSuccessful) {
                     return FormValidation.ok("Connection was successful.");
                 }
                 return FormValidation.error("Error connecting: " + client.getException().getLocalizedMessage());
             } catch (Exception e) {
-                return FormValidation.error("Client error :  "+ e.getMessage());
+                return FormValidation.error("Client error :  " + e.getMessage());
             }
         }
 
         public static Map<String, String> toMap(String value) throws IOException {
-            if(value == null) {
+            if (value == null) {
                 return null;
             }
             Properties properties = new Properties();
